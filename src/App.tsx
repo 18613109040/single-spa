c85c6dea0b5c1df04a2594c01e013cd2fe77e357
hdl <1158025556@qq.com>
2021-06-15 17:20:34 +0800

    feat: 新增hashchange
    测试hashchange

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,30 @@
  * :copyright: (c) 2021, Tungee
  * :date created: 2021-05-17 11:46:28
  * :last editor: 胡东亮
- * :date last edited: 2021-06-15 17:04:24
+ * :date last edited: 2021-06-15 17:19:14
  */
+
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">APP</div>;
+  useEffect(() => {
+    window.addEventListener('hashchange', (e) => {
+      console.log('hashchange', e);
+      const domEle = document.createElement('div');
+      domEle.innerHTML = '<div>add body</div>';
+      document.body.appendChild(domEle);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <div>hash history</div>
+      <a href="#/app1">app1</a>
+      <a href="#/app2">app3</a>
+      <a href="#/app3">app3</a>
+    </div>
+  );
 }
 
 export default App;
