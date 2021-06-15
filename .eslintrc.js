6348108030e98c6f6036246bfd9adfa6cb283f2f
hdl <1158025556@qq.com>
2021-06-15 17:04:49 +0800

    feat: 初始化脚手架

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: [require.resolve("@umijs/fabric/dist/eslint")],
+  rules: {
+    "@typescript-eslint/no-unused-expressions": 0,
+    "no-console": "off"
+  }
+};
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+/*
+ * :file description:
+ * :name: /single-spa/src/App.tsx
+ * :author: 胡东亮
+ * :copyright: (c) 2021, Tungee
+ * :date created: 2021-05-17 11:46:28
+ * :last editor: 胡东亮
+ * :date last edited: 2021-06-15 17:04:24
+ */
+import './App.css';
+
+function App() {
+  return <div className="App">APP</div>;
+}
+
+export default App;
