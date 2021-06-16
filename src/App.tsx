f00df76c0b372dde57d8463b085972f3f240272e
hdl <1158025556@qq.com>
2021-06-16 15:33:02 +0800

    feat: 新增钩子里面获取自定义参数

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@
  * :copyright: (c) 2021, Tungee
  * :date created: 2021-05-17 11:46:28
  * :last editor: 胡东亮
- * :date last edited: 2021-06-16 14:49:04
+ * :date last edited: 2021-06-16 15:31:39
  */
 
 import { useEffect } from 'react';
 import './App.css';
 
 type AppType = {
-  bootstrap?: () => Promise<any>;
-  mount?: () => Promise<any>;
-  unmount?: () => Promise<any>;
+  bootstrap?: (props?: Record<string, any>) => Promise<any>;
+  mount?: (props?: Record<string, any>) => Promise<any>;
+  unmount?: (props?: Record<string, any>) => Promise<any>;
 };
 type RegisterAppConfig = {
   name: string;
@@ -41,8 +41,8 @@ function App() {
       }
     });
     appsToLoad.map(async (load: AppConfig) => {
-      await load.loadApp().bootstrap!();
-      await load.loadApp().mount!();
+      await load.loadApp().bootstrap!(load.customProps);
+      await load.loadApp().mount!(load.customProps);
     });
   };
   // 监听 hashChange 事件
@@ -64,14 +64,16 @@ function App() {
       name: 'app1',
       loadApp: () => {
         return {
-          bootstrap: async () => {
+          bootstrap: async (props?: Record<string, any>) => {
             console.log('应用启动 app1');
+            console.log('获取参数', props);
             domEl = document.createElement('div');
             domEl.id = 'app1';
             document.body.appendChild(domEl);
           },
-          mount: async () => {
+          mount: async (props?: Record<string, any>) => {
             console.log('应用挂载 app1');
+            console.log('获取参数', props);
             domEl!.innerHTML = `App 1 is mounted! <a href=#/app2>Go to app2</a>`;
           },
           unmount: async () => {
@@ -81,20 +83,24 @@ function App() {
         };
       },
       activeWhen: (location: Location) => location?.hash.startsWith('#/app1'),
-      customProps: {},
+      customProps: {
+        name: 'app1',
+      },
     });
     registerApplication({
       name: 'app2',
       loadApp: () => {
         return {
-          bootstrap: async () => {
+          bootstrap: async (props?: Record<string, any>) => {
             console.log('应用启动 app2');
+            console.log('获取参数', props);
             domEl = document.createElement('div');
             domEl.id = 'app2';
             document.body.appendChild(domEl);
           },
-          mount: async () => {
+          mount: async (props?: Record<string, any>) => {
             console.log('应用挂载 app2');
+            console.log('获取参数', props);
             domEl!.innerHTML = `App 1 is mounted! <a href=#/app1>Go to app1</a>`;
           },
           unmount: async () => {
@@ -104,7 +110,9 @@ function App() {
         };
       },
       activeWhen: (location: Location) => location?.hash.startsWith('#/app2'),
-      customProps: {},
+      customProps: {
+        name: 'app2',
+      },
     });
     start();
     return () => {
